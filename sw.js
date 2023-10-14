dd740b83ac233b150e8ba7ebf8a7bbf3223c9781
GabrielBottino <gabrielanjosbottino@gmail.com>
2023-10-14 20:45:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -65,16 +65,37 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
+// Exibir notificação e solicitar permissão para instalação
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.waitUntil(
+      self.clients.matchAll().then((clients) => {
+        if (clients.length === 0) {
+          // O site não está aberto, exiba uma notificação
+          self.registration.showNotification('Bem-vindo ao nosso aplicativo!', {
+            body: 'Você pode instalá-lo na tela inicial do seu dispositivo.',
+            icon: 'ios/16.png',
+            actions: [{ action: 'install', title: 'Instalar' }]
+          });
+        }
+      })
+    );
+  }
+});
+
 // Lidar com o clique no botão "Instalar" da notificação
 self.addEventListener('notificationclick', (event) => {
   if (event.action === 'install') {
-    // Use a janela atual para mostrar a janela de instalação
-    clients.matchAll().then((windowClients) => {
-      const client = windowClients[0];
-      if (client && 'navigate' in client) {
-        client.navigate(client.url);
-      }
-    });
+    event.waitUntil(
+      self.registration.showInstallPrompt()
+        .then((outcome) => {
+          if (outcome === 'accepted') {
+            console.log('Usuário aceitou a instalação do app.');
+          } else {
+            console.log('Usuário recusou a instalação do app.');
+          }
+        })
+    );
   }
   event.notification.close();
 });
