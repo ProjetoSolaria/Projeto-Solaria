a9dd26afd5fc533c4468881801b3bd616bdb4426
GabrielBottino <gabrielanjosbottino@gmail.com>
2023-10-15 17:10:36 -0300

    .

--- paginas-html/sw.js
+++ paginas-html/sw.js
@@ -1,7 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
-const offlineFallbackPage = "offline.html";
 const cacheName = "my-site-cache";
 const filesToCache = [
   "/Projeto-Solaria/paginas-html/sobre-o-projeto.html",
@@ -12,23 +10,15 @@ const filesToCache = [
   "/Projeto-Solaria/codigo-js/menu.js"
 ];
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        return cache.addAll(filesToCache);
-      })
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
 self.addEventListener('activate', (event) => {
-  // Limpe caches antigos aqui
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -43,74 +33,9 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        const cache = await caches.open(cacheName);
-        const cachedResp = await cache.match(event.request);
-
-        if (cachedResp) {
-          return cachedResp;
-        }
-
-        const networkResp = await fetch(event.request);
-
-        if (networkResp && networkResp.status === 200) {
-          cache.put(event.request, networkResp.clone());
-        }
-
-        return networkResp;
-      })()
-    );
-  }
-});
-
-// Exibir notificação e solicitar permissão para instalação
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.waitUntil(
-      self.clients.matchAll().then((clients) => {
-        if (clients.length === 0) {
-          // O site não está aberto, exiba uma notificação
-          self.registration.showNotification('Bem-vindo ao nosso aplicativo!', {
-            body: 'Você pode instalá-lo na tela inicial do seu dispositivo.',
-            icon: 'ios/16.png',
-            actions: [{ action: 'install', title: 'Instalar' }]
-          });
-        }
-      })
-    );
-  }
-});
-
-// Lidar com o clique no botão "Instalar" da notificação
-self.addEventListener('notificationclick', (event) => {
-  if (event.action === 'install') {
-    event.waitUntil(
-      self.registration.showInstallPrompt()
-        .then((outcome) => {
-          if (outcome === 'accepted') {
-            console.log('Usuário aceitou a instalação do app.');
-          } else {
-            console.log('Usuário recusou a instalação do app.');
-          }
-        })
-    );
-  }
-  event.notification.close();
-});
-
-self.addEventListener('beforeinstallprompt', (event) => {
-  // Exibe um popup de instalação
-  event.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('Usuário aceitou a instalação do app.');
-    } else {
-      console.log('Usuário recusou a instalação do app.');
-    }
-  });
-});
-
-self.addEventListener('appinstalled', (event) => {
-  console.log('O aplicativo foi instalado.', event);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
