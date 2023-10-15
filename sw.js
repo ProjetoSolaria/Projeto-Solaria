ef94c5dce63653e3a16db58c90074d1a694325dc
GabrielBottino <gabrielanjosbottino@gmail.com>
2023-10-14 22:22:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,66 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const cacheName = 'my-site-cache';
-const offlineFallbackPage = '/Projeto-Solaria/paginas-html/offline.html';
+const CACHE = "pwabuilder-page";
+const offlineFallbackPage = "/Projeto-Solaria/paginas-html/offline.html";
+const cacheName = "my-site-cache";
 
-self.addEventListener('install', (event) => {
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll([
-        '/Projeto-Solaria/paginas-html/sobre-o-projeto.html',
-        '/Projeto-Solaria/paginas-html/como-funciona.html',
-        '/Projeto-Solaria/paginas-html/inscricoes.html',
-        '/Projeto-Solaria/estilos-css/estilo-main.css',
-        '/Projeto-Solaria/estilos-css/estilo-artigos.css',
-        '/Projeto-Solaria/codigo-js/menu.js'
-      ]);
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Limpe caches antigos aqui
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => {
+          return name !== cacheName;
+        }).map((name) => {
+          return caches.delete(name);
+        })
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((fetchResponse) => {
-        return caches.open(cacheName).then((cache) => {
-          if (!fetchResponse || fetchResponse.status !== 200) {
-            return cache.match(offlineFallbackPage);
-          }
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        });
-      }).catch(() => {
-        return caches.match(offlineFallbackPage);
-      });
-    })
+    (async () => {
+      try {
+        const networkResp = await fetch(event.request);
+
+        if (networkResp && networkResp.status === 200) {
+          // Se a solicitação foi bem-sucedida, atualize o cache com a resposta
+          const cache = await caches.open(cacheName);
+          cache.put(event.request, networkResp.clone());
+        }
+
+        return networkResp;
+      } catch (error) {
+        // Se houver um erro ao buscar na rede, tente buscar no cache
+        const cache = await caches.open(cacheName);
+        const cachedResp = await cache.match(event.request);
+
+        if (cachedResp) {
+          return cachedResp;
+        }
+
+        // Se a solicitação não estiver no cache, retorne a página offline
+        if (event.request.mode === 'navigate') {
+          return caches.match(offlineFallbackPage);
+        }
+      }
+    })()
   );
 });
