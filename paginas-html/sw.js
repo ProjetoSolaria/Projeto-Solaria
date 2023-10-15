a9793c966562f2c84d6d35369d550bf8bbf48aa4
GabrielBottino <gabrielanjosbottino@gmail.com>
2023-10-15 18:37:38 -0300

    Update sw.js

--- paginas-html/sw.js
+++ paginas-html/sw.js
@@ -2,12 +2,12 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const cacheName = "my-site-cache";
 const filesToCache = [
-  "/Projeto-Solaria/paginas-html/sobre-o-projeto.html",
-  "/Projeto-Solaria/paginas-html/como-funciona.html",
-  "/Projeto-Solaria/paginas-html/inscricoes.html",
-  "/Projeto-Solaria/estilos-css/estilo-main.css",
-  "/Projeto-Solaria/estilos-css/estilo-artigos.css",
-  "/Projeto-Solaria/codigo-js/menu.js"
+  "/sobre-o-projeto.html",
+  "/como-funciona.html",
+  "/inscricoes.html",
+  "/estilos-css/estilo-main.css",
+  "/estilos-css/estilo-artigos.css",
+  "/codigo-js/menu.js"
 ];
 
 self.addEventListener("message", (event) => {
